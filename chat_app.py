ff8eda972e5e90a846a6177a7fdd6892c6081bf2
praeterusdice <your-email@example.com>
2026-08-31 09:02:50 -0400

    added admin only page

--- chat_app.py
+++ chat_app.py
@@ -395,15 +395,15 @@ def handle_delete_message(data):
 
 
 
-    class Donator(UserMixin):
-    def __init__(self, id, name, email, is_admin=False):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.is_admin = is_admin
+class Donator(UserMixin):
+def __init__(self, id, name, email, is_admin=False):
+    self.id = id
+    self.name = name
+    self.email = email
+    self.is_admin = is_admin
 
-    def get_id(self):
-        return str(self.id)
+def get_id(self):
+    return str(self.id)
 
 
 @login_manager.user_loader
